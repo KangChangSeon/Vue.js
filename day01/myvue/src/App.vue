54cb4d97439e572ae3ee8583b67e7a4c57ac415e
KangChangSeon <rlckdtjs@naver.com>
2025-05-07 11:32:17 +0900

    0507

--- day01/myvue/src/App.vue
+++ day01/myvue/src/App.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return {
-      gender: "male",
+      inputItem: "",
     };
   },
 
@@ -20,12 +20,6 @@ export default {
 
 <template>
   <form id="loginForm">
-    <label for="male">
-      <input type="radio" v-model="gender" value="male" />남성
-    </label>
-    <label for="female">
-      <input type="radio" v-model="gender" value="female" />여성
-    </label>
-    <button type="button" @click="printData">확인</button>
+    <input type="text" v-model="inputItem" />{{ inputItem }}
   </form>
 </template>
