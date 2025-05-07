b1c17253b5db6f9d4e6fa29cda8c86ba7b1e85b6
KangChangSeon <rlckdtjs@naver.com>
2025-05-07 12:34:23 +0900

    0507

--- day01/myvue/src/App.vue
+++ day01/myvue/src/App.vue
@@ -2,24 +2,62 @@
 export default {
   data() {
     return {
-      inputItem: "",
+      // inputStr: "",
+      arr: [0, 1, 2],
+      obj: { name: "Seon" },
+      primaryColor: "red",
+      primaryStyle: "italic",
     };
   },
 
-  methods: {
-    login() {
-      console.log(`id : ${this.uid}`);
-      console.log(`pw : ${this.upw}`);
+  watch: {
+    // inputStr(newValue, oldValue) {
+    // console.log(`old value : ${oldValue}`);
+    // console.log(`new value : ${newValue}`);
+    // },
+
+    arr: {
+      handler(newValue, oldValue) {
+        console.log(`old value : ${oldValue}`);
+        console.log(`new value : ${newValue}`);
+      },
+      deep: true,
     },
-    printData() {
-      console.log(`gender : ${this.gender}`);
+
+    obj: {
+      handler(oldValue, newValue) {
+        console.log(`old value : ${JSON.stringify(oldValue)}`);
+        console.log(`new value : ${JSON.stringify(newValue)}`);
+      },
+      deep: true,
     },
   },
+
+  computed: {},
+
+  methods: {},
 };
 </script>
 
 <template>
-  <form id="loginForm">
-    <input type="text" v-model="inputItem" />{{ inputItem }}
-  </form>
+  <!-- <input type="text" v-model="inputStr" /> -->
+  <h1 style="color: blue; font-style: italic">{{ arr }}</h1>
+  <h1>{{ obj }}</h1>
+
+  <button @click="arr.push(3)">배열 변경</button>
+  <button @click="obj.age = 20">객체 변경</button>
+
+  <h1 :style="{ color: primaryColor, fontStyle: primaryStyle }">
+    inline style
+  </h1>
+  <h1>Internal Style</h1>
+  <h2>CSS Style</h2>
 </template>
+
+<style scoped>
+@import "~/main.css";
+h1 {
+  color: green;
+  font-style: italic;
+}
+</style>
---
+++ day01/test/App.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      firstName: "Kang",
+      lastName: "ChangSeon",
+      numArr: [1, 2, 3, 4, 5],
+    };
+  },
+
+  computed: {
+    fullName() {
+      console.log("computed fullname");
+      return `${this.firstName} ${this.lastName}`;
+    },
+    evenSum() {
+      return this.numArr
+        .filter((v) => v % 2 !== 0)
+        .reduce((acc, cur) => acc + cur, 0);
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>{{ fullName }}</h1>
+  <h2>
+    {{ numArr.filter((v) => v % 2 === 0).reduce((acc, cur) => acc + cur, 0) }}
+  </h2>
+  <h2>{{ evenSum }}</h2>
+</template>
